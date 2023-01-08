e7fdfb37461c4716f894e25883bfc659ed28b83f
Ericson Nascimento <ericson@grupocyber.com.br>
2023-01-08 15:26:29 -0300

    sistema de login e senha

--- shop/admin/forms.py
+++ shop/admin/forms.py
@@ -10,3 +10,7 @@ class RegistrationForm(Form):
     ])
     confirm = PasswordField('Repita a senha')
     
+class LoginForm(Form):
+    email = StringField('Endereço de email', [validators.Length(min=6, max=35), validators.Email("Endereço de email inválido!")])
+    password = PasswordField('Nova senha', [validators.DataRequired()])
+    
--- shop/admin/routes.py
+++ shop/admin/routes.py
@@ -1,12 +1,16 @@
 from flask import render_template, session, request, redirect, url_for, flash
 from shop import app, db, bcrypt
 
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from .models import User
 
-@app.route('/')
-def home():
-    return render_template('admin/index.html', tittle="Admin Page")
+@app.route('/admin')
+def admin():
+    if 'email' not in session:
+        flash(f'Por favor faça o login', 'atenção!')
+        return redirect(url_for('login'))
+    #products = Addproduct.query.all()
+    return render_template('admin/index.html', tittle="Admin Page") #products=products)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -20,3 +24,16 @@ def register():
         flash(f'Bem Vindo {form.name.data}, Obrigado por se registrar, sucesso!')
         return redirect(url_for('home'))
     return render_template('admin/register.html', form=form, title="Registration Page")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            session['email'] = form.email.data
+            flash(f'Ben vindo {form.email.data} Você está logado agora', 'sucesso!')
+            return redirect(request.args.get('next') or url_for('admin'))
+        else:
+            flash('Email ou senha inválidos, por favor tente novamente', 'atenção!')
+    return render_template('admin/login.html', form=form, tittle='Login Page')
